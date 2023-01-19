ea1ea23e196f5fe3a441aea43e7f1bf4921ee3a1
Dave136 <davejs136@gmail.com>
2023-01-18 22:03:23 -0400

    fix: tweetsFollowers json response

--- models/tweetsFollowers.go
+++ models/tweetsFollowers.go
@@ -6,13 +6,15 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type tweet struct {
+	ID      string    `bson:"_id" json:"_id,omitempty"`
+	Message string    `bson:"message" json:"message,omitempty"`
+	Date    time.Time `bson:"date" json:"date,omitempty"`
+}
+
 type TweetsFollowers struct {
 	ID             primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
 	UserId         string             `bson:"userId" json:"userId,omitempty"`
 	UserRelationId string             `bson:"userRelationId" json:"userRelationId,omitempty"`
-	Tweet          struct {
-		ID      string    `bson:"_id" json:"_id,omitempty"`
-		Message string    `bson:"message" json:"message,omitempty"`
-		Date    time.Time `bson:"date" json:"date,omitempty"`
-	}
+	Tweet          tweet              `json:"tweet"`
 }
